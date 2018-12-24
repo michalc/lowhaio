a32ed9e72f2350f9d2c6001dddb238104534a544
Michal Charemza <michal@charemza.name>
2018-12-24 15:54:00 +0000

    (feat) Initial framework

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='lowhaio',
+    version='0.0.1',
+    author='Michal Charemza',
+    author_email='michal@charemza.name',
+    description='Lightweight and dependency-free Python asyncio HTTP/1.1 client. ',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michalc/lowhaio',
+    py_modules=[
+        'lowhaio',
+    ],
+    python_requires='>=3.7.0',
+    test_suite='test',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: AsyncIO',
+    ],
+)
