ef9d19aec7b7a290f141a781db40bd6dd1ca47f5
Michal Charemza <michal@charemza.name>
2018-12-27 08:36:37 +0000

    (refactor) Less nesting in cleanup

--- lowhaio.py
+++ lowhaio.py
@@ -25,28 +25,37 @@ async def connection_pool(loop):
 
     @asynccontextmanager
     async def connection(hostname, ip_address, port, ssl_context):
+
+        async def cleanup_sock_close():
+            sock.close()
+
+        async def cleanup_sock_shutdown():
+            sock.shutdown(SHUT_RDWR)
+
+        async def cleanup_ssl_unwrap():
+            nonlocal sock
+            sock = await ssl_unwrap_socket(loop, ssl_sock) if ssl_sock else sock
+
+        cleanups = [cleanup_ssl_unwrap, cleanup_sock_shutdown, cleanup_sock_close]
+
         sock = socket(family=AF_INET, type=SOCK_STREAM, proto=IPPROTO_TCP)
         sock.setblocking(False)
         ssl_sock = None
 
         try:
             await loop.sock_connect(sock, (ip_address, port))
+
             ssl_sock = ssl_context.wrap_socket(
                 sock, server_hostname=hostname, do_handshake_on_connect=False)
             await ssl_handshake(loop, ssl_sock)
 
             yield Connection(ssl_sock)
         finally:
-            try:
+            for cleanup in cleanups:
                 try:
-                    sock = await ssl_unwrap_socket(loop, ssl_sock) if ssl_sock else sock
-                finally:
-                    try:
-                        sock.shutdown(SHUT_RDWR)
-                    finally:
-                        sock.close()
-            except BaseException:
-                pass
+                    await cleanup()
+                except BaseException:
+                    pass
 
     yield ConnectionPool(connection=connection)
 
