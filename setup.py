eb1728c253a0ad61b79f322ebf0df99872776412
Michal Charemza <michal@charemza.name>
2019-04-27 22:28:49 +0100

    (release) v0.0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='lowhaio',
-    version='0.0.5',
+    version='0.0.6',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='Lightweight Python asyncio HTTP/1.1 client. ',
